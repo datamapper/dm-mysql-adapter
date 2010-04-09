28eb7c1d438521d45e94ca2696f91dd1d3a8eb53
snusnu <gamsnjaga@gmail.com>
2010-04-09 02:26:39 +0200

    No more need to require an explicit automigration extension
    See http://is.gd/bkM8K

--- spec/adapter_spec.rb
+++ spec/adapter_spec.rb
@@ -1,12 +1,11 @@
 require 'spec_helper'
 
-require 'dm-migrations/adapters/dm-mysql-adapter'
-require 'dm-mysql-adapter'
+require 'dm-migrations'
 
 require 'dm-core/spec/adapter_shared_spec'
 require 'dm-do-adapter/spec/shared_spec'
 
-describe DataMapper::Adapters::MysqlAdapter do
+describe "DataMapper::Adapters::MysqlAdapter" do
 
   before :all do
     @adapter    = DataMapper.setup(:default, 'mysql://localhost/dm_core_test')
